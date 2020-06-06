a23c4673ee2a59b45bc766df07aa208ee146e31e
eug-vs <eug-vs@keemail.me>
2020-06-06 22:33:28 +0300

    feat: create initial theme

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,34 @@
 import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
+
+import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
 import { CssBaseline } from '@material-ui/core';
+import teal from '@material-ui/core/colors/teal';
 import 'typeface-roboto';
 
 import Header from './Header/Header';
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: teal[700],
+    },
+    text: {
+      primary: '#000000',
+      secondary: 'rgba(255, 255, 255, 0.6)',
+    },
+  },
+});
 
 const App: React.FC = () => {
   const [page, setPage] = useState('feed');
 
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <CssBaseline />
       <Header page={page} setPage={setPage} />
       <h1> Hello, world! </h1>
-    </>
+    </ThemeProvider>
   );
 };
 
