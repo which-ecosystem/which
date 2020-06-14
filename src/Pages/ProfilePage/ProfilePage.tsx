e1c44e40680d52cd1862e795fc7fbf4ab4af929f
ilyayudovin <ilyayudovin123@gmail.com>
2020-06-14 18:07:14 +0300

    add feed to profile

--- src/Pages/ProfilePage/ProfilePage.tsx
+++ src/Pages/ProfilePage/ProfilePage.tsx
@@ -2,15 +2,19 @@ import React from 'react';
 import { User } from '../../types';
 import SignInForm from './SignInForm';
 import ProfileInfo from './ProfileInfo';
+import Feed from "../../Components/Feed/Feed";
 
 interface PropTypes {
   setUser: (newUser: User | undefined) => void;
   user: User | undefined;
 }
 
-const ProfilePage: React.FC<PropTypes> = ({ setUser, user }) => {
+const ProfilePage: React.FC<PropTypes> = ({ setUser, user}) => {
   return (
-    user ? <ProfileInfo id={user?._id || ''} setUser={setUser} />
+    user ? <>
+      <ProfileInfo id={user?._id} setUser={setUser} />
+      <Feed page={'Feed'}/>
+      </>
       : <SignInForm setUser={setUser} />
   );
 };
