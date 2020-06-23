bff39941e2da37db8d971b2dc1b84bce8b1fbbc2
eug-vs <eug-vs@keemail.me>
2020-06-23 13:27:28 +0300

    feat: add auth headers to reqeusts

--- src/requests.ts
+++ src/requests.ts
@@ -1,10 +1,12 @@
-import axios, { AxiosResponse } from 'axios';
+import axios from 'axios';
 
-type Request = (url: string, data?: Record<string, unknown>) => Promise<AxiosResponse>;
+const requests = axios.create({
+  baseURL: 'http://localhost:3030',
+  headers: {
+    'Authorization': localStorage.getItem('token')
+  }
+});
 
-const baseApiUrl = 'http://localhost:3030';
-
-export const get: Request = url => axios.get(baseApiUrl + url);
-export const del: Request = url => axios.delete(baseApiUrl + url);
-export const post: Request = (url, data) => axios.post(baseApiUrl + url, data);
+export const { get, post, put } = requests;
+export default requests;
 
