0204f48a00543ea876efc83260f244f7c14d8a82
eug-vs <eug-vs@keemail.me>
2020-06-25 13:40:03 +0300

    fix: listen to token changes in requests

--- src/requests.ts
+++ src/requests.ts
@@ -2,9 +2,11 @@ import axios from 'axios';
 
 const requests = axios.create({
   baseURL: 'http://localhost:3030',
-  headers: {
-    'Authorization': localStorage.getItem('token')
-  }
+});
+
+requests.interceptors.request.use(config => {
+  config.headers.Authorization = localStorage.getItem('token');
+  return config;
 });
 
 export const { get, post, put } = requests;
