8c5d7716e18e1da9a6556c581dc8056b7c182ae8
eug-vs <eug-vs@keemail.me>
2020-06-26 15:08:23 +0300

    build: connect to the new API

--- src/requests.ts
+++ src/requests.ts
@@ -1,9 +1,11 @@
 import axios from 'axios';
 import _ from 'lodash';
 
-const requests = axios.create({
-  baseURL: 'http://localhost:3030'
-});
+const baseURL = process.env.NODE_ENV === 'production'
+  ? 'https://which-api.herokuapp.com'
+  : 'http://localhost:3030';
+
+const requests = axios.create({ baseURL });
 
 requests.interceptors.request.use(config => {
   const token = localStorage.getItem('token');
